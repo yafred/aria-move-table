5577650627f96694a4b9c3af1fbd4bad8c951285
yafred <12477220+yafred@users.noreply.github.com>
2025-05-16 13:05:04 +0200

    test

--- README.md
+++ README.md
@@ -1,10 +1,7 @@
 - npm init -y
 - npm install typescript esbuild chess.js snabbdom
 
-
-- npx esbuild index.ts --bundle --outfile=doc/bundle.js
-
-
+- npx esbuild index.ts --bundle --outfile=docs/bundle.js
 
 - npm install -g http-server
 - http-server -c-1
--- index.ts
+++ index.ts
@@ -1,4 +1,85 @@
-import { v4 as uuidv4 } from 'uuid';
+import { h, init, VNode, classModule, attributesModule, eventListenersModule, styleModule } from 'snabbdom';
 
-const id = uuidv4();
-console.log("Generated UUID:", id);
+interface Move {
+  turn: number;
+  ply: number;
+  color: 'white' | 'black';
+  san: string;
+  advantage: string;
+  movetime: string;
+}
+
+const renderSan = (san: string) => {
+  if (san.charAt(1) !== '-') return san.charAt(0) + '.' + san.substring(1);
+};
+
+const patch = init([classModule, attributesModule, eventListenersModule, styleModule]);
+const container = document.getElementById('container');
+
+let vnode: VNode;
+let dataset: Move[];
+
+function redraw() {
+  vnode = patch(vnode || container, h('main', [renderTable1(), renderTable2()]));
+}
+
+function renderTable1(): VNode {
+  return h('div', [
+    h('h2#moveTableLabel1', 'Move table using plain html table'),
+    h(
+      'table.moves',
+      h('tbody', [
+        h('tr', [h('th', 'Turn'), h('th', 'Move'), h('th', 'Move time'), h('th', 'White advantage')]),
+        ...dataset.map(item => {
+          return h('tr', [
+            h('td', item.turn),
+            h('td', item.color + ' played ' + renderSan(item.san)),
+            h('td', item.movetime + ' seconds'),
+            h('td', item.advantage),
+          ]);
+        }),
+      ]),
+    ),
+  ]);
+}
+
+function renderTable2(): VNode {
+  return h('div', [
+    h('h2#moveTableLabel2', 'Move table using html table with aria'),
+    h(
+      'table.moves',
+      {
+        attrs: {
+          role: 'grid',
+          'aria-labelledBy': 'moveTableLabel2',
+          'aria-rowcount': dataset.length,
+          'aria-colcount': 4,
+        },
+      },
+
+      h('tbody', [
+        h('tr', [
+          h('th', { attrs: { 'aria-colindex': 1 } }, 'Turn'),
+          h('th', { attrs: { 'aria-colindex': 2 } }, 'Move'),
+          h('th', { attrs: { 'aria-colindex': 3 } }, 'Move time'),
+          h('th', { attrs: { 'aria-colindex': 4 } }, 'White advantage'),
+        ]),
+        ...dataset.map(item => {
+          return h('tr', { attrs: { 'aria-rowindex': item.ply } }, [
+            h('td', { attrs: { tabindex: -1 } }, item.turn),
+            h('td', { attrs: { tabindex: -1 } }, item.color + ' played ' + renderSan(item.san)),
+            h('td', { attrs: { tabindex: -1 } }, item.movetime + ' seconds'),
+            h('td', { attrs: { tabindex: -1 } }, item.advantage),
+          ]);
+        }),
+      ]),
+    ),
+  ]);
+}
+
+fetch('data.json')
+  .then(response => response.json())
+  .then(json => {
+    dataset = json;
+    redraw();
+  });
